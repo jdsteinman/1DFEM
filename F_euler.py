a57c08dba0bc78f54fbfa33ff87320e9d25dc4a5
jdsteinman <johnsteinman@utexas.edu>
2020-12-10 21:12:02 -0600

    Add forward and backward euler

--- F_euler.py
+++ F_euler.py
@@ -1,4 +1,4 @@
-# 1D Finite Element Code
+# 1D Heat Equation Forward Euler
 import numpy as np
 import numexpr as ne
 import math
@@ -6,49 +6,49 @@ from scipy.linalg import inv
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=3)
 
-# Intialize and Preallocate
+# Inputs
 Ne = 10   # Number of Elements
 N = Ne+1  # Number of Nodes
 
-f = '(pi**2-1)*e**(-t)*sin(pi*x)'
-u_guess = 'sin(pi*x)'
-u_act = 'e**(-t)*sin(pi*x)'
-global_expr = {'pi': math.pi, 'e': math.e}
+f = '(pi**2-1)*e**(-t)*sin(pi*x)' # RHS of eqn
+u_guess = 'cos(pi*x)'             # Initial guess
 
-K = np.zeros((N,N))    # N by N global stiffness matrix
-M = np.zeros((N,N))    # N by N global mass matrix
-k_local = np.zeros((2,2))    # local stiffness matrix
-m_local = np.zeros((2,2))    # local mass matrix
-
-g_w = np.array([1., 1.])  # Gaussian quadrature weights
-g_xi = np.array([-1/math.sqrt(3), 1/math.sqrt(3)])  # Gaussian quadrature points in xi space
-
-psi_1 = '(1-xi)/2'
-psi_2 = '(1+xi)/2'
-dpsi_1 = -1/2
-dpsi_2 = 1/2
-dpsi = np.array([-1/2, 1/2])
-psi_1_quad = np.array(ne.evaluate(psi_1, local_dict={'xi': g_xi}))
-psi_2_quad = np.array(ne.evaluate(psi_2, local_dict={'xi': g_xi}))
-psi_quad = np.vstack((psi_1_quad, psi_2_quad))
-
-# mesh
+# Mesh
 x = np.linspace(0,1,N)
 h = np.diff(x)
-dbc = np.array(([0, 0], [N-1,0]))
+dbc = np.array(([0, 0], [N-1,0])) # [node, value]
 
-# time discretization
-dt = 1/551
+# Time discretization
+dt = 1/560
 time = np.arange(0,1+dt,dt)
 
-# connectivity
+# Connectivity
 map_lg = np.arange(Ne)   
 map_lg = np.vstack((map_lg, map_lg+1)).T
 
+# Integration tools
+g_w = np.array([1., 1.])  # Gaussian quadrature weights
+g_xi = np.array([-1/math.sqrt(3), 1/math.sqrt(3)])  # Gaussian quadrature points in xi space
+
+# Basis Functions
+psi_1 = '(1-xi)/2'
+psi_2 = '(1+xi)/2'
+dpsi = np.array([-1/2, 1/2])
+psi_1_quad = np.array(ne.evaluate(psi_1, local_dict={'xi': g_xi}))  # basis function vals at quad pts
+psi_2_quad = np.array(ne.evaluate(psi_2, local_dict={'xi': g_xi}))
+psi_quad = np.vstack((psi_1_quad, psi_2_quad))
+
+# Initialize and Preallocate
+global_expr = {'pi': math.pi, 'e': math.e}  # global dictionary for functions
+K = np.zeros((N,N))          # N by N global stiffness matrix
+M = np.zeros((N,N))          # N by N global mass matrix
+k_local = np.zeros((2,2))    # local stiffness matrix
+m_local = np.zeros((2,2))    # local mass matrix
+
 # Start Method
-for k in range(Ne):      # loop through elements
-    for l in range(2):   # loop over nodes on element k
-        for m in range(2):   # mass and stiffness
+for k in range(Ne):    # loop through elements
+    for l in range(2):    # loop over nodes on element k
+        for m in range(2):  
             m_local[l][m] = h[k]/2*np.sum(psi_quad[l,:]*psi_quad[m,:]*g_w)
             k_local[l][m] = 2/h[k]*dpsi[l]*dpsi[m]*2
     
@@ -68,27 +68,18 @@ M_inv_copy = M_inv.copy()
 for i in range(N):
     if(i in dbc[:,0]):
         M[i,:] = np.zeros(N)
-        # M[:,i] = np.zeros(N)
+        M[:,i] = np.zeros(N)
         K[i,:] = np.zeros(N)
-        # K[:,i] = np.zeros(N)
+        K[:,i] = np.zeros(N)
 
         M_inv[i,:] = np.zeros(N)
         M_inv[:,i] = np.zeros(N)
         M_inv[i,i] = 1
 
-np.savetxt('M.txt', M, fmt='%4.2f', delimiter=' ')
-np.savetxt('K.txt', K, fmt='%4.2f', delimiter=' ')
-np.savetxt('M_inv.txt', M_inv, fmt='%4.2f', delimiter=' ')
-
-open('F.txt', 'w').close()
-open('u.txt', 'w').close()
-
 # Solve in time
 u = ne.evaluate(u_guess, local_dict = {'x':x}, global_dict=global_expr)
 
-iter = 0
 for t in time:
-    iter += 1
     # Find F vector
     F = np.zeros(N)
 
@@ -110,37 +101,21 @@ for t in time:
                 if(i != j):
                     F[i] -= M_inv_copy[i,j] * dbc[idx,1]/dt 
    
-    with open('F.txt', 'a') as file:
-        np.savetxt(file, F.reshape((1,N)), fmt='%4.2f', delimiter=' ')      
-        
-    with open('u.txt', 'a') as file:
-        np.savetxt(file, u.reshape((1,N)), fmt='%4.2f', delimiter=' ')       
-
-    A = dt*M_inv@K
-    B = dt*M_inv@F
-
-    # if iter == 10:
-    #     with open('A.txt', 'w') as file:
-    #         np.savetxt(file, A, fmt='%4.2f', delimiter=' ')      
-            
-    #     with open('B.txt', 'w') as file:
-    #         np.savetxt(file, B, fmt='%4.2f', delimiter=' ') 
     u_new = M_inv@( (M-dt*K)@u + dt*F)
-    # u_new = u-A@u+B
     u = u_new
 
-fig, ax = plt.subplots(nrows = 2, ncols = 1)
-ax[0].plot(x, u)
-ax[0].grid(True)
-ax[0].set_xlabel('x')
-ax[0].set_ylabel('u')
-ax[0].set_title('FEM solution at t=1')
-
+# Plotting
 x_real = np.linspace(0,1,100)
-u_real = ne.evaluate(u_act, local_dict = {'x': x_real, 't': 1}, global_dict=global_expr)
-ax[1].plot(x_real, u_real)
-ax[1].grid(True)
-ax[1].set_xlabel('x')
-ax[1].set_ylabel('u')
-ax[1].set_title('Analytical solution at t=1')
+u_real = ne.evaluate('e**-t*sin(pi*x)', local_dict = {'x': x_real, 't': 1}, global_dict=global_expr)
+
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+ax.plot(x_real, u_real, 'r', label = 'real')
+ax.plot(x, u, 'b', label = 'calculated')
+ax.grid(True)
+ax.set_xlabel('x')
+ax.set_ylabel('u')
+ax.set_title('Comparing Solutions')
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels)
+
 plt.show()
